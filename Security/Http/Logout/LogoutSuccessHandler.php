d5c5a88ba705c6336cc1867c00db8a5a14313f0e
hslavich <hernan.slavich@gmail.com>
2014-11-17 11:31:13 -0300

    Logout handler

---
+++ Security/Http/Logout/LogoutSuccessHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hslavich\SimplesamlphpBundle\Security\Http\Logout;
+
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Router;
+
+class LogoutSuccessHandler implements LogoutSuccessHandlerInterface
+{
+    protected $auth;
+    protected $router;
+
+    public function __construct($auth, Router $router)
+    {
+        $this->auth = $auth;
+        $this->router = $router;
+    }
+
+    public function onLogoutSuccess(Request $request)
+    {
+        $returnTo = $request->headers->get('referer');
+        $request->getSession()->invalidate();
+
+        return new RedirectResponse($this->auth->getLogoutURL($returnTo));
+    }
+}
